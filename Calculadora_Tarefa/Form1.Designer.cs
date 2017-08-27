af40aa324019935b8f674a05743ee21f4f7a7b06
Emmy <emilene.llopes@gmail.com>
2017-08-27 19:08:22 -0300

    Tarefa Calculadora

---
+++ Calculadora_Tarefa/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Calculadora_Tarefa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textValor01 = new System.Windows.Forms.TextBox();
+            this.textValor02 = new System.Windows.Forms.TextBox();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.botaoSoma = new System.Windows.Forms.Button();
+            this.botaoMultiplicacao = new System.Windows.Forms.Button();
+            this.botaoSubtracao = new System.Windows.Forms.Button();
+            this.botaoDivisao = new System.Windows.Forms.Button();
+            this.botaoLimpar = new System.Windows.Forms.Button();
+            this.botaoSair = new System.Windows.Forms.Button();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(112, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULADORA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(12, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Valor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(12, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total:";
+            // 
+            // textValor01
+            // 
+            this.textValor01.BackColor = System.Drawing.Color.SeaShell;
+            this.textValor01.Location = new System.Drawing.Point(71, 126);
+            this.textValor01.Name = "textValor01";
+            this.textValor01.Size = new System.Drawing.Size(185, 20);
+            this.textValor01.TabIndex = 6;
+            // 
+            // textValor02
+            // 
+            this.textValor02.BackColor = System.Drawing.Color.SeaShell;
+            this.textValor02.Location = new System.Drawing.Point(71, 178);
+            this.textValor02.Name = "textValor02";
+            this.textValor02.Size = new System.Drawing.Size(185, 20);
+            this.textValor02.TabIndex = 7;
+            // 
+            // textTotal
+            // 
+            this.textTotal.BackColor = System.Drawing.Color.SeaShell;
+            this.textTotal.Location = new System.Drawing.Point(71, 226);
+            this.textTotal.Name = "textTotal";
+            this.textTotal.Size = new System.Drawing.Size(185, 20);
+            this.textTotal.TabIndex = 8;
+            // 
+            // botaoSoma
+            // 
+            this.botaoSoma.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoSoma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoSoma.Location = new System.Drawing.Point(292, 75);
+            this.botaoSoma.Name = "botaoSoma";
+            this.botaoSoma.Size = new System.Drawing.Size(75, 23);
+            this.botaoSoma.TabIndex = 9;
+            this.botaoSoma.Text = "+";
+            this.botaoSoma.UseVisualStyleBackColor = false;
+            this.botaoSoma.Click += new System.EventHandler(this.botaoSoma_Click);
+            // 
+            // botaoMultiplicacao
+            // 
+            this.botaoMultiplicacao.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoMultiplicacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoMultiplicacao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoMultiplicacao.Location = new System.Drawing.Point(292, 178);
+            this.botaoMultiplicacao.Name = "botaoMultiplicacao";
+            this.botaoMultiplicacao.Size = new System.Drawing.Size(75, 23);
+            this.botaoMultiplicacao.TabIndex = 12;
+            this.botaoMultiplicacao.Text = "*";
+            this.botaoMultiplicacao.UseVisualStyleBackColor = false;
+            this.botaoMultiplicacao.Click += new System.EventHandler(this.botaoMultiplicacao_Click);
+            // 
+            // botaoSubtracao
+            // 
+            this.botaoSubtracao.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoSubtracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoSubtracao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoSubtracao.Location = new System.Drawing.Point(292, 129);
+            this.botaoSubtracao.Name = "botaoSubtracao";
+            this.botaoSubtracao.Size = new System.Drawing.Size(75, 23);
+            this.botaoSubtracao.TabIndex = 13;
+            this.botaoSubtracao.Text = "-";
+            this.botaoSubtracao.UseVisualStyleBackColor = false;
+            this.botaoSubtracao.Click += new System.EventHandler(this.botaoSubtracao_Click);
+            // 
+            // botaoDivisao
+            // 
+            this.botaoDivisao.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoDivisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoDivisao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoDivisao.Location = new System.Drawing.Point(292, 226);
+            this.botaoDivisao.Name = "botaoDivisao";
+            this.botaoDivisao.Size = new System.Drawing.Size(75, 23);
+            this.botaoDivisao.TabIndex = 14;
+            this.botaoDivisao.Text = "/";
+            this.botaoDivisao.UseVisualStyleBackColor = false;
+            this.botaoDivisao.Click += new System.EventHandler(this.botaoDivisao_Click);
+            // 
+            // botaoLimpar
+            // 
+            this.botaoLimpar.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoLimpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoLimpar.Location = new System.Drawing.Point(71, 291);
+            this.botaoLimpar.Name = "botaoLimpar";
+            this.botaoLimpar.Size = new System.Drawing.Size(75, 23);
+            this.botaoLimpar.TabIndex = 15;
+            this.botaoLimpar.Text = "Limpar";
+            this.botaoLimpar.UseVisualStyleBackColor = false;
+            this.botaoLimpar.Click += new System.EventHandler(this.botaoLimpar_Click);
+            // 
+            // botaoSair
+            // 
+            this.botaoSair.BackColor = System.Drawing.Color.SeaShell;
+            this.botaoSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoSair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.botaoSair.Location = new System.Drawing.Point(181, 291);
+            this.botaoSair.Name = "botaoSair";
+            this.botaoSair.Size = new System.Drawing.Size(75, 23);
+            this.botaoSair.TabIndex = 16;
+            this.botaoSair.Text = "Sair";
+            this.botaoSair.UseVisualStyleBackColor = false;
+            this.botaoSair.Click += new System.EventHandler(this.botaoSair_Click);
+            // 
+            // textNome
+            // 
+            this.textNome.BackColor = System.Drawing.Color.SeaShell;
+            this.textNome.Location = new System.Drawing.Point(71, 78);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(185, 20);
+            this.textNome.TabIndex = 17;
+            this.textNome.TextChanged += new System.EventHandler(this.textNome_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(387, 336);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.botaoSair);
+            this.Controls.Add(this.botaoLimpar);
+            this.Controls.Add(this.botaoDivisao);
+            this.Controls.Add(this.botaoSubtracao);
+            this.Controls.Add(this.botaoMultiplicacao);
+            this.Controls.Add(this.botaoSoma);
+            this.Controls.Add(this.textTotal);
+            this.Controls.Add(this.textValor02);
+            this.Controls.Add(this.textValor01);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tarefa Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textValor01;
+        private System.Windows.Forms.TextBox textValor02;
+        private System.Windows.Forms.TextBox textTotal;
+        private System.Windows.Forms.Button botaoSoma;
+        private System.Windows.Forms.Button botaoMultiplicacao;
+        private System.Windows.Forms.Button botaoSubtracao;
+        private System.Windows.Forms.Button botaoDivisao;
+        private System.Windows.Forms.Button botaoLimpar;
+        private System.Windows.Forms.Button botaoSair;
+        private System.Windows.Forms.TextBox textNome;
+    }
+}
+
---
+++ Calculadora_Tarefa/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Tarefa
+{
+    public partial class Form1 : Form
+    {
+        String nome;
+        double valor01, valor02, resultado;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botaoSubtracao_Click(object sender, EventArgs e)
+        {
+            valor01 = Convert.ToDouble(textValor01.Text);
+            valor02 = Convert.ToDouble(textValor02.Text);
+
+            resultado = valor01 - valor02;
+
+            textTotal.Text = nome + resultado.ToString();
+        }
+
+        private void botaoMultiplicacao_Click(object sender, EventArgs e)
+        {
+            valor01 = Convert.ToDouble(textValor01.Text);
+            valor02 = Convert.ToDouble(textValor02.Text);
+
+            resultado = valor01 * valor02;
+
+            textTotal.Text = nome + resultado.ToString();
+        }
+
+        private void botaoDivisao_Click(object sender, EventArgs e)
+        {
+            valor01 = Convert.ToDouble(textValor01.Text);
+            valor02 = Convert.ToDouble(textValor02.Text);
+
+            resultado = valor01 / valor02;
+
+            textTotal.Text = nome + string.Format("{0:0.00}", resultado);
+        }
+
+        private void botaoLimpar_Click(object sender, EventArgs e)
+        {
+            textNome.Clear();
+            textValor01.Clear();
+            textValor02.Clear();
+            textTotal.Clear();
+        }
+
+        private void botaoSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textNome_TextChanged(object sender, EventArgs e)
+        {
+            nome = textNome.Text + " o resultado é: ";
+        }
+
+        private void botaoSoma_Click(object sender, EventArgs e)
+        {
+            valor01 = Convert.ToDouble(textValor01.Text);
+            valor02 = Convert.ToDouble(textValor02.Text);
+
+            resultado = valor01 + valor02;
+
+            textTotal.Text = nome + resultado.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
